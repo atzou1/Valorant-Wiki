4cbd21f7a63cb6631ad72bcab90e8ee6f3dc7b54
Alyson Tzou <alysontzou@gmail.com>
2022-05-13 00:43:54 -0400

    search by displayName

--- Valorant API/js/main.js
+++ Valorant API/js/main.js
@@ -2,46 +2,58 @@
 document.querySelector('button').addEventListener('click', getValorantInfo)
 
 //grab info from API 
-function getValorantInfo(){
+function getValorantInfo() {
   let agentUuid = document.querySelector('input').value
 
-fetch(`https://valorant-api.com/v1/agents/${agentUuid}`)
+
+  let thingy = [{ "name": "John" }, { "name": "Rose" }];
+
+  fetch(`https://valorant-api.com/v1/agents?isPlayableCharacter=true`)
     .then(res => res.json())
     .then(data => {    //data means to "come back" from valorant-api
-       //IMG
-       document.querySelector('img').src = data.data.fullPortraitV2
-      //NAME
-      document.querySelector('span').innerText = data.data.displayName
-      //ROLE
-      document.querySelector('span.role').innerText = data.data.role.displayName
+      console.log(data.data)
+      //loops through to find displayName
+      for (let i = 0; i < data.data.length; i++) { 
+        // if below is true, a match has been found
+        if (data.data[i].displayName.toLowerCase() === agentUuid.toLowerCase()) { //[i] is used in data to loop through each index in data 
+          //IMG
+          document.querySelector('img').src = data.data[i].fullPortraitV2
+          //NAME
+          document.querySelector('span').innerText = data.data[i].displayName
+          //ROLE
+          document.querySelector('span.role').innerText = data.data[i].role.displayName
 
-      //DESCRIPTION
-      document.querySelector('p').innerText = data.data.description
+          //DESCRIPTION
+          document.querySelector('p').innerText = data.data[i].description
 
-      //ABILITIES
-      //ability1
-      document.querySelector('h4.ability1').innerText = data.data.abilities[0].displayName
-      document.querySelector('p.moveset-desc1').innerText = data.data.abilities[0].description
-      document.querySelector('img.ability1').src = data.data.abilities[0].displayIcon
-      //ability2
-      document.querySelector('h4.ability2').innerText = data.data.abilities[1].displayName
-      document.querySelector('p.moveset-desc2').innerText = data.data.abilities[1].description
-      document.querySelector('img.ability2').src = data.data.abilities[1].displayIcon
-      //ability3
-      document.querySelector('h4.ability3').innerText = data.data.abilities[2].displayName
-      document.querySelector('p.moveset-desc3').innerText = data.data.abilities[2].description
-      document.querySelector('img.ability3').src = data.data.abilities[2].displayIcon
-      //ultimate
-      document.querySelector('h4.ultimate').innerText = data.data.abilities[3].displayName
-      document.querySelector('p.moveset-desc4').innerText = data.data.abilities[3].description
-      document.querySelector('img.ultimate').src = data.data.abilities[3].displayIcon
+          //ABILITIES
+          //ability1
+          document.querySelector('h4.ability1').innerText = data.data[i].abilities[0].displayName
+          document.querySelector('p.moveset-desc1').innerText = data.data[i].abilities[0].description
+          document.querySelector('img.ability1').src = data.data[i].abilities[0].displayIcon
+          //ability2
+          document.querySelector('h4.ability2').innerText = data.data[i].abilities[1].displayName
+          document.querySelector('p.moveset-desc2').innerText = data.data[i].abilities[1].description
+          document.querySelector('img.ability2').src = data.data[i].abilities[1].displayIcon
+          //ability3
+          document.querySelector('h4.ability3').innerText = data.data[i].abilities[2].displayName
+          document.querySelector('p.moveset-desc3').innerText = data.data[i].abilities[2].description
+          document.querySelector('img.ability3').src = data.data[i].abilities[2].displayIcon
+          //ultimate
+          document.querySelector('h4.ultimate').innerText = data.data[i].abilities[3].displayName
+          document.querySelector('p.moveset-desc4').innerText = data.data[i].abilities[3].description
+          document.querySelector('img.ultimate').src = data.data[i].abilities[3].displayIcon
 
-       //VOICELINE
-       document.querySelector('audio').src = data.data.voiceLine.mediaList[0].wave
-      })
-    .catch(err => {
-        console.log(`error ${err}`)
-    })};
+          //VOICELINE
+          document.querySelector('audio').src = data.data[i].voiceLine.mediaList[0].wave
+          // Below is a sentinel value that stops the loop because a match was found
+          i = data.data.length + 1;
+        }
+      }
+    }).catch(err => {
+      console.log(`error ${err}`)
+    })
+};
 
 
 
